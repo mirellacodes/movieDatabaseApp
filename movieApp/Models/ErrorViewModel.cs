2b72feb6f9cb642a757dcdb0858a6d2411c56f6f
Mirella <mirellacodes@gmail.com>
2023-10-04 20:52:08 +0100

    created a new project

---
+++ movieApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace movieApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
